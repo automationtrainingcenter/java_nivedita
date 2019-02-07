f7bc5d6158ce041703a17c2375857f01dfada6b2
automation <atcsurya@gmail.com>
2019-02-07 11:08:11 +0530

    initial upload

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world ");
+		System.out.println("welcome to selenium course");
+		System.out.println("with Java programming");
+		System.out.println();
+	}
+
+}
