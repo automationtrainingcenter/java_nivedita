7cd80f2e5519831ca66c1d733c903f65f8eeddd0
automation <atcsurya@gmail.com>
2019-02-08 11:10:56 +0530

    comments in Java

---
+++ src/basics/Comments.java
@@ -0,0 +1,21 @@
+package basics;
+
+/**
+ this class explains comments in Java
+ There are 3 type of comment in Java
+ single line comments
+ Multiline comments
+ Documentation comments
+ */
+
+// single line comment
+
+/*
+ * this is
+ * multiline
+ * comment
+ */
+
+public class Comments {
+
+}
